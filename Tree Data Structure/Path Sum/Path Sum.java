5c8788630c4101dd1fa4f1af7e0ff69d4a159522
asatulsinha4 <35336564+asatulsinha4@users.noreply.github.com>
2021-05-10 23:22:02 +0530

    Update Path Sum.java

--- Tree Data Structure/Path Sum/Path Sum.java
+++ Tree Data Structure/Path Sum/Path Sum.java
@@ -1,4 +1,23 @@
+/*
 
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Example :
+
+Given the below binary tree and sum = 22,
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+*/
 
 /**
  * Definition for binary tree
@@ -22,7 +41,7 @@ public class Solution {
     public void helper(TreeNode node,int sum, int target){
         if(node!=null && ans!=1){
             sum = sum+node.val;
-            if(sum==target && node.left==null && node.right==null) ans = 1;
+            if(sum==target && node.left==null && node.right == null) ans = 1;
             helper(node.left,sum,target);
             helper(node.right,sum,target);
         }
